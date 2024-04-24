8306765365b869b29f6b65c03ee776e1ce4f64eb
DapperMongoose <11383629+DapperMongoose@users.noreply.github.com>
2024-04-23 19:57:19 -0500

    completed processing and apply_force module

--- player.gd
+++ player.gd
@@ -3,5 +3,10 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	apply_impulse(Vector2(25, 0))
+	pass
 
+func _process(delta):
+	pass
+
+func _physics_process(delta):
+	apply_force(Vector2(25, 0))
